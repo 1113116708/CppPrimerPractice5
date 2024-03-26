75fdce73ee853759934b432741fa3246a3c4977e
kaisong.jin <13770411224@163.com>
2024-03-26 15:32:44 +0800

    更新

--- ch02/ch02.cpp
+++ ch02/ch02.cpp
@@ -18,8 +18,38 @@ void ex_2_8(){
     std::cout << 2 << "\t" << "M" << std::endl;
 }
 
+void test(){
+    double dval = 1;
+    double *pd = &dval;
+    double *pd2 = pd;
+    // * 是解引用符号
+    std::cout << "pd = " << pd << std::endl;
+    std::cout << "*pd = " << *pd << std::endl;
+    std::cout << "pd2 = " << pd2 << std::endl;
+    std::cout << "*pd2 = " << *pd2 << std::endl;
+
+    // 位*pd赋值就是为pd所指向的对象赋值
+    *pd = 2;
+    std::cout << "dval = " << dval << std::endl;
+    std::cout << "pd = " << pd << std::endl;
+    std::cout << "*pd = " << *pd << std::endl;
+}
+
+void ex_2_18(){
+    int a = 1;
+    int b = 2;
+    int *p = &a;
+    std::cout << "p = " << p << std::endl;
+    p = &b;
+    std::cout << "change p = " << p << std::endl;
+    *p = 99;
+    std::cout << "change b = " << b << std::endl;
+}
+
 int main(){
     // ex_2_4();
-    ex_2_8();
+    // ex_2_8();
+    // test();
+    ex_2_18();
     return 0;
 }
